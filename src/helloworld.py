b4816d0e8abf32f71e555c49c64a5e2ee5ae4ffa
senseiKazuhiro <kzhr201006@gmail.com>
2025-02-05 06:34:22 +0900

    条件分岐を学習

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,7 +1,10 @@
-# http://tinyurl.com/h79ob7s
-
-x = 15
-print( x )
-
-x = 30
-print( x )
+age = 2
+if age > 5:
+    print("messege")
+elif age < 3:
+    print("athor mseege")
+elif age < 2: 
+    print("diffferent messege")
+else:
+    print("else messege")
+      
--- src/othello.py
+++ src/othello.py
@@ -20,9 +20,28 @@ clock = pygame.time.Clock()
 White = (255, 255, 255)
 Black = (0, 0, 0)
 Red = (255, 0, 0)
-Green = (0, 255, 0)
+Green = (0, 130, 0 )
 Blue = (0, 0, 255)
 Yellow = (255, 255, 0)
+
+#盤面(黒 : 1, 白　: -1,)
+board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 1, -1, 0, 0, 0, ],
+        [0, 0, 0, -1, 1, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ]]
+
+for row in board:
+    for col in row:
+        if col == 1:
+            print("黒です")
+        elif col == -1:
+            print("白です")    
+
 
 #メインループ========================================
 run = True
@@ -30,6 +49,8 @@ while run:
 
 #背景の塗りつぶし
     screen.fill(Green)
+
+
 
 #マスの描画 
     for i in range(square_num):
@@ -41,24 +62,34 @@ while run:
 
 #盤面(黒 : 1, 白　: -1,)
 board = [
-    [0, 0, 0, 0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, 0, 0, 0, ],
-    [0, 0, 0, 1, -1, 0, 0, 0, ],
-    [0, 0, 0, -1, 1, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, 0, 0, 0, ]]
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 1, -1, 0, 0, 0, ],
+        [0, 0, 0, -1, 1, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ],
+        [0, 0, 0, 0, 0, 0, 0, 0, ]]
+
+#盤面の描画
+for row_index, row in enumerate(board):
+    for col_index, col in enumerate(row):
+        if col == 1:
+            pygame.draw.circle(screen, Black, (col_index*square_size + 50, row_index*square_size + 50), 45)
+        elif col == -1:
+            pygame.draw.circle(screen, White, (col_index*square_size + 50, row_index*square_size + 50), 45)
+
+               
 
 #イベントの取得 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
 #画面の更新
-    pygame.display.update()
-    clock.tick(FPS)
+pygame.display.update()
+clock.tick(FPS)
 
-#===================================================
 
+#=============================================================
 pygame.quit()
