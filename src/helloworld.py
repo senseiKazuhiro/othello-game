f87913aea387520095362e737387377a26f578dd
senseiKazuhiro <kzhr201006@gmail.com>
2025-02-06 20:24:12 +0900

    盤面、グリッド線の描画の処理を関数化

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,6 +1,2 @@
-age = input("Enter your age please:")
-int_age = int(age)
-if int_age > 18:
-    print("aha, you are an adult!")
-else:
-    print("you are still a kid !")
+def even_odd(text):
+    
--- src/othello.py
+++ src/othello.py
@@ -2,6 +2,31 @@ import pygame
 
 pygame.init()
 
+#関数
+
+#グリッド線の描画
+def draw_grid():
+    for i in range(square_num):
+        #横線
+        pygame.draw.line(screen, Black, (0, i*square_size), (screen_width, i*square_size), 3)
+
+        #縦線
+        pygame.draw.line(screen, Black, (i*square_size, 0), (i*square_size, screen_height), 3)
+
+#盤面の描画
+def draw_board():
+    for row_index, row in enumerate(board):
+        for col_index, col in enumerate(row):
+            if col == 1:
+                pygame.draw.circle(screen, Black, (col_index * square_size + 50, row_index * square_size + 50), 30)
+            elif col == -1:
+                pygame.draw.circle(screen, White, (col_index * square_size + 50, row_index * square_size + 50), 30)
+                 
+            
+        
+
+
+
 #ゲーム画面の作成
 screen_width = 800
 screen_height = 800
@@ -35,14 +60,6 @@ board = [
         [0, 0, 0, 0, 0, 0, 0, 0, ],
         [0, 0, 0, 0, 0, 0, 0, 0, ]]
 
-for row in board:
-    for col in row:
-        if col == 1:
-            print("黒です")
-        elif col == -1:
-            print("白です")    
-
-
 #メインループ========================================
 run = True
 while run:
@@ -50,46 +67,23 @@ while run:
 #背景の塗りつぶし
     screen.fill(Green)
 
-
-
-#マスの描画 
-    for i in range(square_num):
-        #横線
-        pygame.draw.line(screen, Black, (0, i*square_size), (screen_width, i*square_size), 3)
-        
-        #縦線
-        pygame.draw.line(screen, Black, (i*square_size, 0), (i*square_size, screen_height), 3)
-
-#盤面(黒 : 1, 白　: -1,)
-board = [
-        [0, 0, 0, 0, 0, 0, 0, 0, ],
-        [0, 0, 0, 0, 0, 0, 0, 0, ],
-        [0, 0, 0, 0, 0, 0, 0, 0, ],
-        [0, 0, 0, 1, -1, 0, 0, 0, ],
-        [0, 0, 0, -1, 1, 0, 0, 0, ],
-        [0, 0, 0, 0, 0, 0, 0, 0, ],
-        [0, 0, 0, 0, 0, 0, 0, 0, ],
-        [0, 0, 0, 0, 0, 0, 0, 0, ]]
+#グリッド線の描画
+    draw_grid()
 
 #盤面の描画
-for row_index, row in enumerate(board):
-    for col_index, col in enumerate(row):
-        if col == 1:
-            pygame.draw.circle(screen, Black, (col_index*square_size + 50, row_index*square_size + 50), 45)
-        elif col == -1:
-            pygame.draw.circle(screen, White, (col_index*square_size + 50, row_index*square_size + 50), 45)
+    draw_board()
 
-               
+
+
 
 #イベントの取得 
-for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
 #画面の更新
-pygame.display.update()
-clock.tick(FPS)
-
+    pygame.display.update()
+    clock.tick(FPS)
 
 #=============================================================
 pygame.quit()
